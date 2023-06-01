6e47d2e97e9a9ddd0edc700c460aacc728497b52
Ismail Ali <ismailchangezi801@gmail.com>
2023-06-01 16:58:28 +0500

    Create required classes and fix linter errors

--- main.rb
+++ main.rb
@@ -7,8 +7,8 @@ require_relative 'classes/capitalize_decorator'
 require_relative 'classes/trimmer_decorator'
 
 person = Person.new(22, 'maximilianus')
- puts person.correct_name
- puts capitalizedPerson = CapitalizeDecorator.new(person)
- puts capitalizedPerson.correct_name
- puts capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
- puts capitalizedTrimmedPerson.correct_name
+puts person.correct_name
+puts capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+puts capitalized_trimmed_person = TrimmerDecorator.new(capitalizedPerson)
+puts capitalized_trimmed_person.correct_name
